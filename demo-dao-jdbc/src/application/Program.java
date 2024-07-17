d56647caaf9722e5688def45ac63ca5e7ac501ed
Miguel Piva <miguel.piva@outlook.com>
2024-07-16 22:29:30 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Program {
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bruce", "bruce@outlook.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
+        
         System.out.println(seller);
     }
 }

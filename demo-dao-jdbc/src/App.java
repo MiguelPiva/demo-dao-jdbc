202a28a863a3e66f7e67c3f787fb666111a8a1a7
Miguel Piva <miguel.piva@outlook.com>
2024-07-16 21:55:06 -0300

    Creating project and git repository

---
+++ demo-dao-jdbc/src/App.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import db.DB;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world!");
+    }
+}
